9f6911e945152e03f78e1f24b30434e85b1f5036
Babajide Williams <babanila@yahoo.com>
2022-06-23 21:43:12 +0200

    Rename folder and files

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "baba-core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/baba-core/"
             ),
           },
         ],
--- packages/acme-utils/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @acme/utils
-
-## 0.0.1
-
-### Patch Changes
-
-- 2c4ee3c: God on point
